5f721e606fdb28570d82d7f25e8f17f4ad663818
Mattia Ambrosini <mattiambrosini@gmail.com>
2020-07-23 21:28:20 +0200

    Versione Finale 1.0

--- controllo_disco_rigido_script.m
+++ controllo_disco_rigido_script.m
@@ -6,6 +6,12 @@ close all
 b = 0.002;
 J = 0.001;
 
+% Setpoint e ritardo puro
+y0 = 300;
+delay = 0.3;
+s = tf('s');
+ritardo = exp(-delay*s);
+
 %% DICHIARO LE MATRICI DEL SISTEMA
 A = [-b/J, 0;1, 0];
 B = [1/J; 0];
@@ -13,10 +19,10 @@ C = [0, 1];
 D = 0;
 
 %% NOME DEI MODELLI SIMULINK
-op_mdl = 'controllo_disco_rigido_openloop';
-cld_mdl = 'controllo_disco_rigido_disturbi';
-cln_mdl = 'controllo_disco_rigido_rumore';
-fn_mdl = 'controllo_disco_rigido_final';
+mdl_A = 'controllo_disco_rigido_openloop';
+mdl_1 = 'controllo_disco_rigido_disturbi';
+mdl_2 = 'controllo_disco_rigido_rumore';
+mdl_3 = 'controllo_disco_rigido_ritardo';
 
 %% DEFINISCO IL MODELLO IN SPAZIO DI STATO ED IN FUNZIONE DI TRASFERIMENTO
 sys_ss = ss(A,B,C,D);
@@ -36,10 +42,10 @@ disp("Uscita")
 disp(ybar)
 
 %% RISPOSTA A SCALINO DEL SISTEMA
-t = 0:0.01:10;
+t = 0:0.01:5;
 [ys, ts] = step(sys_tf, t);
 
-MdlStep = sim(op_mdl);
+MdlStep = sim(mdl_A);
 figure, plot(MdlStep.tout, MdlStep.y), grid on
 title("Risposta allo scalino - Modello")
 
@@ -83,9 +89,6 @@ for i = 1:1:size(mag,1)
 end
 disp(bw)
 
-% figure, semilogx(w(1:i), mag(1:i)), grid on
-% title("Diagramma di Bode del modulo della funzione per la banda stimata")
-
 %% ANALISI SISTEMA SENZA CONTROLLORE
 z = zero(sys_tf);
 p = pole(sys_tf);
@@ -102,15 +105,10 @@ figure, bode(sys_tf), grid on
 title("Diagramma di Bode della FdT ad anello aperto")
 
 %% CALCOLO E PLOTTO I LIMITI DA IMPORRE AL PROBLEMA 
-
 %Inserisco i dati del problema
 wc = 10; wd = 0.5:0.1:5; wn = 50:10:500;
 d = 1; n = 1;
 Ed = 20*log10(10); En = 0;
-
-% Setpoint e ritardo puro
-y0 = 300;
-delay = 0.3;
 
 PHm = 65;
 disp("Margine di fase minnimo richiesto")
@@ -118,21 +116,17 @@ disp(PHm)
 disp("Margine di fase del sistema non controllato")
 disp(Pm)
 
-%Grafico le soglie
-figure
-semilogx(w,mag)
-hold on
-semilogx(wc,0,'g*'),semilogx(wd,Ed, 'ko'),semilogx(wn,En, 'r>')
-ylabel('dB'),xlabel('pulsazione'),grid;
-title('Diagramma di Bode - Modulo')
-
 %% CALCOLO LE CARATTERISTICHE DEL REGOLATORE
-R1 = 100;
-R2_num = [1 2];
-R2_den = [1 1400];
+gsys = cell2mat(sys_tf.num);
+R1 = 10^(round((((Ed)+20*wd(end))/20)-1))/gsys(3);
+R2_zero = [-2];
+R2_pole = [-500];
+
+R2_num = s-R2_zero;
+R2_den = s-R2_pole;
 
 disp("FdT del regolatore")
-R = tf(R2_num*R1, R2_den)
+R = series(R1,(R2_num/R2_den))
 
 disp("FdT d'anello")
 L = series(sys_tf,R)
@@ -160,22 +154,13 @@ title("Diagramma di Bode della FdT ad anello chiuso")
 
 %% VERIFICO LA STABILITA' DEL NUOVO SISTEMA
 figure, nyquist(feedback(L,1)), grid on
-zL = real(zero(1+L));
+title("Diagramma di Nyquist - L")
+
 disp("Radici di 1+L(s)")
-disp(zL)
-cas = 0;
-for i = 1:1:size(zL,1)
-    if zL(i,1) >= 0
-        disp("Sistema non as. stabile")
-        cas = 1;
-    end
-end
-if cas == 0
-    disp("Sistema in anello chiuso as. stabile")
-end
+ztf(L)
 
 %% VERIFICO IL COMPORTAMENTO DEL SISTEMA IN ANELLO CHIUSO
-MdlCL = sim(cld_mdl);
+MdlCL = sim(mdl_1);
 figure, plot(MdlCL.tout, MdlCL.y), grid on
 title("Risposta del modello in anello chiuso con disturbi")
 
@@ -184,12 +169,15 @@ figure, plot(tcl, ycl), grid on
 title("Risposta allo scalino - FdT anello chiuso")
 
 %% CALCOLO SOLUZIONE PER IL RUMORE
-R1_n = 10000;
-R2_n_num = [1 2];
-R2_n_den = [1 1540 196000];
-R2_n = tf(R2_n_num, R2_n_den);
+R1_n = 35000;
+R2_zero_n = [-2];
+R2_pole_n = [-500, -1400];
+
+R2_num_n = s-R2_zero_n;
+R2_den_n = (s-R2_pole_n(1))*(s-R2_pole_n(2));
+
 disp("Regolatore con rumore e disturbi")
-R_noise = series(R2_n, R1_n)
+R_noise = series(R1_n,(R2_num_n/R2_den_n))
 
 disp("FdT d'anello con rumore e disturbi")
 L_noise = series(R_noise,sys_tf)
@@ -213,23 +201,14 @@ disp(PmL_n)
 
 %% VERIFICO STABILITA' DI QUESTA SOLUZIONE
 figure,nyquist(feedback(L_noise,1)),grid on
-zL_n = real(zero(1+L_noise));
+title("Diagramma di Nyquist - L noise")
+
 disp("Radici di 1+L_noise(s)")
-disp(zL_n)
-cas = 0;
-for i = 1:1:size(zL_n,1)
-    if zL_n(i,1) >= 0
-        disp("Sistema non as. stabile")
-        cas = 1;
-    end
-end
-if cas == 0
-    disp("Sistema in anello chiuso as. stabile")
-end
+ztf(L_noise)
 
 %% VERIFICO IL COMPORTAMENTO DEL SISTEMA IN ANELLO CHIUSO
-Mdlcl = sim(cln_mdl);
-figure, plot(Mdlcl.tout, Mdlcl.yd), grid on
+Mdlcl = sim(mdl_2);
+figure, plot(Mdlcl.tout, Mdlcl.y), grid on
 title("Risposta del modello in anello chiuso con rumore")
 
 [ycl_n, tcl_n] = step(feedback(L_noise,1), t);
@@ -237,22 +216,55 @@ figure, plot(tcl_n, ycl_n), grid on
 title("Risposta allo scalino - FdT anello chiuso")
 
 %% CALCOLO SOLUZIONE PER IL RITARDO PURO
-s = tf('s');
-ritardo = exp(-delay*s);
-disp("FdT d'anello con predittore di Smith")
-M = series(series(feedback(R_noise,series(1-ritardo,sys_tf)),sys_tf),ritardo)
+L_delay = L_noise*ritardo;
+[y_r, t_r] = step(feedback(L_delay,1), t);
+figure, plot(t_r, y_r), grid on
+title("Effetto del ritardo")
+figure, nyquist(feedback(L_delay,1)),grid on
 
-figure, bode(feedback(M,1)), grid on
-figure, nyquist(feedback(M,1)), grid on
+%predittore di Smith
+M = series(series(feedback(R_noise,series(1-ritardo,sys_tf)),sys_tf),ritardo);
+
+%soluzione con polo e k
+W_pole = max(R2_pole_n);
+k = (1/67.5)*gsys(3);
+W = k/(s-W_pole);
+N = series(W, L_delay);
+
+%% VERIFICO LA STABILITA' DELLA SOLUZIONE
+figure, nyquist(feedback(N,1)),grid on
+title("Diagramma di Nyquist - N")
+
+disp("Radici di 1+N(s)")
+ztf(N)
 
 %% VERIFICO IL COMPORTAMENTO FINALE DEL MODELLO
-MdlFnl = sim(fn_mdl);
-figure, plot(MdlFnl.tout, MdlFnl.yd), grid on
-title("Risposta del modello in anello chiuso con ritardo")
+MdlFnl = sim(mdl_3);
+figure, plot(MdlFnl.tout, MdlFnl.y_p), grid on
+hold on, plot(MdlFnl.tout, MdlFnl.y), legend ('Predittore Smith','Polo e k')
+title("Risposta del modello - Ritardo")
 
-[yfl, tfl] = step(feedback(M,1),t);
-figure, plot(tfl, yfl), grid on
-title("Risposta allo scalino - FdT anello chiuso")
+[y_p, t_p] = step(feedback(M,1),t);
+[y_w, t_w] = step(feedback(N,1),t);
+figure, plot(t_p, y_p), grid on, hold on, plot(t_w, y_w)
+title("Risposta allo scalino - Ritardo")
+legend ('Predittore Smith','Polo e k')
 
 %% APRO I MODELLI SIMULINK
-open_system(op_mdl),open_system(cld_mdl),open_system(cln_mdl),open_system(fn_mdl)
+open_system(mdl_A),open_system(mdl_1),open_system(mdl_2),open_system(mdl_3)
+
+%% FUNZIONE CALCOLO ZERI DI 1+TF(S)
+function ztf(tf)
+    z = real(zero(1+tf));
+    disp(z)
+    cas = 0;
+    for i = 1:1:size(z,1)
+        if z(i,1) >= 0
+            disp("Sistema non as. stabile")
+            cas = 1;
+        end
+    end
+    if cas == 0
+        disp("Sistema in anello chiuso as. stabile")
+    end
+end
